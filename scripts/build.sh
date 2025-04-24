d45c315eb01dabae41cb0ce11b25723be111cd3c
Toni Kuschan <toni.kuschan@voange.com>
2025-04-24 19:05:52 +0200

    init

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
- 
+
 npm ci
+
+internalsecret="$(openssl rand -hex 64)"
+vcr secret create --name "VCR_MESSAGES_QUEUE_INTERNAL_AUTH_KEY" --value $internalsecret
+if [ $? -eq 0 ]; then 
+    echo "Created new internal secret."; 
+else
+    echo "Secret already exists. Nothing to be done."; 
+fi
